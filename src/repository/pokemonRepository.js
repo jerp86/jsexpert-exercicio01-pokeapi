85db8d54ef0a26be600370a1ffb2f2231bc7c960
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-03-19 16:43:46 -0300

    feat: create methods

--- src/repository/pokemonRepository.js
+++ src/repository/pokemonRepository.js
@@ -4,6 +4,36 @@ class PokemonRepository {
   constructor() {
     this.api = new PokemonApi();
   }
+
+  async getAll() {
+    const response = await this.api.get('/pokemon')
+
+    return {
+      totalOptions: response.count,
+      list: response.results,
+    }
+  }
+
+  async getById(id) {
+    console.log("🚀 ~ file: pokemonRepository.js:18 ~ PokemonRepository ~ getById ~ id:", id)
+    const idIsEmpty = !id || id?.length === 0
+    const idIsNan = isNaN(id)
+    console.log("🚀 ~ file: pokemonRepository.js:20 ~ PokemonRepository ~ getById ~ idIsNan:", idIsNan)
+    if (idIsEmpty || idIsNan) {
+      throw new Error('Hey, I need ID to search anywhere Pokémon')
+    }
+
+    const { name, moves } = await this.api.get(`/pokemon/${id}`)
+
+    const filteredMoves = moves.slice(0, 3).map((moveObj) => moveObj.move.name)
+
+    return {
+      id,
+      name,
+      moves: filteredMoves,
+      formattedName: `#${id} - ${name}`,
+    }
+  }
 }
 
 module.exports = PokemonRepository;
