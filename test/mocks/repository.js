6dcad9fec651d3facd219e01ce343b23fa06bb72
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-03-19 16:41:29 -0300

    feat: add mocks

---
+++ test/mocks/repository.js
@@ -0,0 +1,16 @@
+const urls = {
+  pokemons: 'https://pokeapi.co/api/v2/pokemon',
+  pokemon1: 'https://pokeapi.co/api/v2/pokemon/5',
+  pokemon2: 'https://pokeapi.co/api/v2/pokemon/22',
+  pokemon3: 'https://pokeapi.co/api/v2/pokemon/86',
+};
+
+const mocks = {
+  pokemonList: require('./valid-pokemon-list.json'),
+  pokemon1: require('./valid-pokemon-1.json'),
+  pokemon2: require('./valid-pokemon-2.json'),
+  pokemon3: require('./valid-pokemon-3.json'),
+  team: require('./valid-team.json'),
+};
+
+module.exports = { mocks, urls }
