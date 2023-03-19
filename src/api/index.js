3bc5f16829948f6c16178480042f8e590a804d33
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-03-19 16:42:32 -0300

    feat: add app and pokémon api

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,46 @@
+const https = require('node:https')
+
 const API_BASE_URL = 'https://pokeapi.co/api/v2';
 
 class PokemonApi {
-  async get(url) {
+  async get(url = '') {
+    const preparedUrl = `${API_BASE_URL}${url}`
+    return new Promise((resolve, reject) => {
+      https.get(preparedUrl, res => {
 
+        let body = ''
 
+        res.on('data', chunk => {
+          body += chunk
+        })
+
+        res.on('end', async () => {
+          try {
+            if (!body || body.length === 0) return
+
+            const preparedResponse = await JSON.parse(body)
+            resolve(preparedResponse)
+          } catch (error) {
+            console.error('Aaaaaa, Ash and Dr. Oker, we have a problem!!!')
+            console.log("🚀 ~ file: index.js:18 ~ PokemonApi ~ res.on ~ error:", error)
+            reject(error)
+          }
+        })
+      })
+    })
   }
 }
+
+// ;(async () => {
+//   const api = new PokemonApi()
+//   // const [t1, t2, t3] = await Promise.all([
+//   //   api.get('/pokemon/5'),
+//   //   api.get('/pokemon/22'),
+//   //   api.get('/pokemon/86'),
+//   // ])
+//   const t1 = await api.get('/pokemon')
+//   // console.log("🚀 ~ file: index.js:31 ~ ; ~ response:", t1.name, t2.name, t3.name)
+//   console.log(JSON.stringify(t1))
+// })()
 
 module.exports = PokemonApi;
--- src/app.js
+++ src/app.js
@@ -1,1 +1,60 @@
-console.info('🚀 Your API Running right here!');
+const http = require('node:http')
+const PokemonRepository = require('./repository/pokemonRepository')
+const PokemonService = require('./service/pokemonService')
+
+const DEFAULT_PORT = process.env.PORT ?? 3000
+const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+}
+
+const DEFAULT_SERVICE = () => {
+  const repository = new PokemonRepository()
+  const service = new PokemonService({ repository })
+
+  return { service }
+}
+
+class Api {
+  constructor(dependencies = DEFAULT_SERVICE()) {
+    this.teamService = dependencies.service
+  }
+
+  generateRoutes() {
+    return {
+      default: (_, res) => {
+        res.writeHeader(404, { "Content-Type": "text/html" });
+        res.write("Hey there, try '/team' so we can present to you your's 3 possibles choices")
+        return res.end()
+      },
+
+      '/team:get': async (req, res) => {
+        const team = await this.teamService.getTeam()
+
+        res.writeHeader(200, { "Content-Type": "text/html" });
+        res.write(JSON.stringify(team))
+        return res.end()
+      },
+    }
+  }
+
+  handler(req, res) {
+    const { url, method } = req
+    const routeKey = `${url}:${method.toLowerCase()}`
+    const routes = this.generateRoutes()
+
+    const chosen = routes[routeKey] ?? routes.default
+
+    res.writeHeader(200, DEFAULT_HEADERS)
+    return chosen(req, res)
+  }
+
+  createServer(port = DEFAULT_PORT) {
+    const app = http
+      .createServer(this.handler.bind(this))
+      .listen(port, () => console.log(`App running at port ${port}`))
+
+    return app
+  }
+}
+
+module.exports = Api
