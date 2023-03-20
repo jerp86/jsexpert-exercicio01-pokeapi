83d5f4dfbc451cfa17d46e2da77b0b0f8b1a81ba
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-03-19 22:41:54 -0300

    fix: adjust urls and mocks

--- test/mocks/repository.js
+++ test/mocks/repository.js
@@ -1,8 +1,8 @@
 const urls = {
   pokemons: 'https://pokeapi.co/api/v2/pokemon',
-  pokemon1: 'https://pokeapi.co/api/v2/pokemon/5',
-  pokemon2: 'https://pokeapi.co/api/v2/pokemon/22',
-  pokemon3: 'https://pokeapi.co/api/v2/pokemon/86',
+  pokemon1: 5,
+  pokemon2: 22,
+  pokemon3: 86,
 };
 
 const mocks = {
@@ -12,6 +12,7 @@ const mocks = {
   pokemon2: require('./valid-pokemon-2.json'),
   pokemon3: require('./valid-pokemon-3.json'),
   team: require('./valid-team.json'),
+  numberListOfId: [5, 22, 86],
 };
 
 module.exports = { mocks, urls }
