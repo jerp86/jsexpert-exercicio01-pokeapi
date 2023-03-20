07cade7a020f06344b5483b6e7e3f41515689273
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-03-19 22:42:59 -0300

    feat: add pokémon service

--- src/service/pokemonService.js
+++ src/service/pokemonService.js
@@ -1,7 +1,33 @@
+const FINAL_POKEMON_ID = 171
+
 class PokemonService {
   constructor({ repository }) {
-    this.repository = repository;
+    if (!repository) {
+      throw new Error('Ash, we have a problem! Where is the repository?')
+    }
+
+    this.repository = repository
+  }
+
+  getId(finalNumber = FINAL_POKEMON_ID) {
+    if (isNaN(finalNumber)) {
+      throw new Error('Ash, we have a problem! We need a final number of Pokémon to draught')
+    }
+
+    return Array
+      .from({ length: 3 })
+      .map(() => Math.floor(Math.random() * finalNumber + 1))
+  }
+
+  async getTeam() {
+    const { totalOptions } = await this.repository.getAll()
+
+    const listId = this.getId(totalOptions)
+
+    const team = await Promise.all(listId.map(async id => await this.repository.getById(id)))
+
+    return team
   }
 }
 
-module.exports = PokemonService;
+module.exports = { PokemonService, FINAL_POKEMON_ID }
